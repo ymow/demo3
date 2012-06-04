ef9e05b0c5917c34779b26c66291167058a36e1d
ymow <kenshin0812@hotmail.com>
2012-06-04 10:52:07 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Demo3::Application.routes.draw do
-  match '/', to: 'static_pages#home'
+  get "users/new"
+ root to: 'static_pages#home'
+
+ match '/signup',  to: 'users#new'
+
+ # match '/', to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 #resources :events
- root to: 'static_pages#home'
+
 #root :to => "welcome#index"
 get "welcome/say_hello" => "welcome#say"
 resources :products do
